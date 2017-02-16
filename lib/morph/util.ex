6cf042c0b47d1ec5e53a4a363820c0546e43f2e7
Stephen Moloney <stephen@stephenmoloney.com>
2017-02-16 22:28:18 +0000

    fix for empty string "" - cmoncrief/elixir-morph/issues/2

--- lib/morph/util.ex
+++ lib/morph/util.ex
@@ -2,11 +2,13 @@ defmodule Morph.Util do
 
   @moduledoc false
 
+  def upper_first(""), do: ""
   def upper_first(input) do
     [head | tail] = String.codepoints input
     to_string [String.upcase(head) | tail]
   end
 
+  def upper_first(""), do: ""
   def lower_first(input) do
     [head | tail] = String.codepoints input
     to_string [String.downcase(head) | tail]
