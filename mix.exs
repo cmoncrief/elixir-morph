9d99cad8f5fc7c910376659a47bf2689516f8f95
Stephen Moloney <stephen@stephenmoloney.com>
2017-04-11 20:36:08 +0100

    add parenthesis

--- mix.exs
+++ mix.exs
@@ -2,16 +2,18 @@ defmodule Morph.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :morph,
-     name: "Morph",
-     source_url: "https://github.com/cmoncrief/elixir-morph",
-     version: "0.1.0",
-     description: description,
-     package: package,
-     elixir: "~> 1.2",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps]
+    [
+      app: :morph,
+      name: "Morph",
+      source_url: "https://github.com/cmoncrief/elixir-morph",
+      version: "0.1.0",
+      description: description(),
+      package: package(),
+      elixir: "~> 1.2",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps()
+    ]
   end
 
   def application do
